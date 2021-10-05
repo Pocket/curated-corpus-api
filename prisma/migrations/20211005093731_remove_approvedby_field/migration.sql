39e3794ca61868b9ee9a8f1c4852ead601fe2aa0
Nina Pypchenko <22447785+nina-py@users.noreply.github.com>
2021-10-06 07:39:03 +1100

    [BACK-1118] Remove approvedBy from database schema (#106)
    - Added a migration to keep the DB and Prisma schema in sync.

---
+++ prisma/migrations/20211005093731_remove_approvedby_field/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `approvedBy` on the `NewTabFeedSchedule` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `NewTabFeedSchedule` DROP COLUMN `approvedBy`;
