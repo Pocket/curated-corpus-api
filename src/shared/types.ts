6df18a10819b7359d1b20ec72bb8cbfb02c1a20f
Jonathan Petto <jpetto@users.noreply.github.com>
2022-01-20 14:27:53 -0600

    chore(shared data): update new tabs (en-GB & en-INTL)
    - also update ORGANIC_TIMESPENT prospectType (was TIMESPENT)

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -1,7 +1,7 @@
 // Pocket Shared Data - see Confluence for details
 export enum ProspectType {
   GLOBAL = 'GLOBAL',
-  TIMESPENT = 'ORGANIC_TIMESPENT',
+  ORGANIC_TIMESPENT = 'ORGANIC_TIMESPENT',
   SYNDICATED = 'SYNDICATED',
 }
 
@@ -23,7 +23,7 @@ export const NewTabs: NewTab[] = [
     utcOffset: -500,
     prospectTypes: [
       ProspectType.GLOBAL,
-      ProspectType.TIMESPENT,
+      ProspectType.ORGANIC_TIMESPENT,
       ProspectType.SYNDICATED,
     ],
   },
@@ -31,7 +31,19 @@ export const NewTabs: NewTab[] = [
     name: 'de-DE',
     guid: 'DE_DE',
     utcOffset: 100,
-    prospectTypes: [ProspectType.GLOBAL],
+    prospectTypes: [ProspectType.GLOBAL, ProspectType.ORGANIC_TIMESPENT],
+  },
+  {
+    name: 'en-GB',
+    guid: 'EN_GB',
+    utcOffset: 0,
+    prospectTypes: [ProspectType.GLOBAL, ProspectType.ORGANIC_TIMESPENT],
+  },
+  {
+    name: 'en-INTL',
+    guid: 'EN_INTL',
+    utcOffset: 530,
+    prospectTypes: [ProspectType.GLOBAL, ProspectType.ORGANIC_TIMESPENT],
   },
 ];
 
