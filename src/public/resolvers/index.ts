8e342fcaab1c0a105985780aff9b269948e68ca6
Mathijs Miermans <m.miermans@gmail.com>
2022-06-03 10:03:12 -0700

    feat(public resolvers): [HS-32] Add CorpusItem resolver

--- src/public/resolvers/index.ts
+++ src/public/resolvers/index.ts
@@ -1,6 +1,7 @@
 import { DateResolver } from 'graphql-scalars';
 import { getScheduledSurface } from './queries/ScheduledSurface';
 import { getItemsForScheduledSurface } from './queries/ScheduledSurfaceItem';
+import { getApprovedItemByExternalId } from '../../database/queries/ApprovedItem';
 
 export const resolvers = {
   // The Date resolver enforces the date to be in the YYYY-MM-DD format.
@@ -8,6 +9,15 @@ export const resolvers = {
   ScheduledSurface: {
     // The `items` subquery pulls in scheduled corpus items for a given date.
     items: getItemsForScheduledSurface,
+  },
+  CorpusItem: {
+    __resolveReference: async (item, { db }) => {
+      const { id } = item;
+
+      /**
+       */
+      return getApprovedItemByExternalId(db, id);
+    },
   },
   Query: {
     // Gets the metadata for a Scheduled Surface (for example, New Tab).
