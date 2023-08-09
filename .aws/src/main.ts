e6ec0acd595df73ae32f8e8ae77707a82fca42d8
Katerina Chinnappan <kchinnappan@mozilla.com>
2023-08-09 09:27:49 -0700

    upgrade terrfarom modules to 4.17.0

--- .aws/src/main.ts
+++ .aws/src/main.ts
@@ -130,7 +130,8 @@ class CuratedCorpusAPI extends TerraformStack {
       subnetIds: pocketVpc.privateSubnetIds,
       rdsConfig: {
         databaseName: 'curation_corpus',
-        masterUsername: 'pkt_curation_corpus',
+        // changed from pkt_curation_corpus to pkt_cur_corpus, see here: https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/USER_CreateDBInstance.html
+        masterUsername: 'pkt_cur_corpus',
         engine: 'aurora-mysql',
         engineMode: 'serverless',
         scalingConfiguration: {
