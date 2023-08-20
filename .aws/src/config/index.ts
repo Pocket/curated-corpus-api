386f3499310a79a192876ef3f8f79528e2d1d954
Mathijs Miermans <mmiermans@users.noreply.github.com>
2023-08-19 17:35:15 -0700

    fix(RDS): Increase minCapacity on production from 1 to 2 (#993)

--- .aws/src/config/index.ts
+++ .aws/src/config/index.ts
@@ -15,7 +15,7 @@ const snowplowEndpoint = isDev
   : 'com-getpocket-prod1.collector.snplow.net';
 
 const rds = {
-  minCapacity: 1,
+  minCapacity: isDev ? 1 : 2,
   maxCapacity: isDev ? 1 : undefined,
 };
 
